5583ac897be95a58dba1f2d0ae74917c36997cb4
cedric ratsimandranto <raymondcedric92@gmail.com>
2025-03-03 16:03:52 +0100

    modéle senior added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_03_142243) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_03_145822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "seniors", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "address"
+    t.string "key_skill"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_seniors_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +37,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_03_142243) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "seniors", "users"
 end
