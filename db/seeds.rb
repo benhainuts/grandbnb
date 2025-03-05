39ecb2bff7af8b19eb0ee3a5e1e696af9911e5fe
sami ezzaher <sam.ezzaher@gmail.com>
2025-03-05 14:40:18 +0100

    hfosb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "open-uri"
 
 puts "creation des seniors"
-Senior.create(name:"Gérard", address:"16 villa gaudelet", city: "paris", key_skill:"bricolage", user_id:"1", age:74, photo:"")
+
+gerard = Senior.create(name:"Gérard", address:"16 villa gaudelet", city: "paris", key_skill:"bricolage", user_id:"1", age:74, photo:"")
+file = URI.parse("https://images.unsplash.com/photo-1542105907-bbbb542a5f79?q=80&w=2104&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+gerard.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+gerard.save
+
 Senior.create(name:"Simone", address:"16 villa gaudelet", city: "paris", key_skill:"ufc", user_id:"1", age:74, photo:"")
+
 Senior.create(name:"Gautier", address:"16 villa gaudelet", city: "paris", key_skill:"L.O.L", user_id:"1", age:74, photo:"")
+
 Senior.create(name:"Sylvie", address:"16 villa gaudelet", city: "paris", key_skill:"krav maga", user_id:"1", age:74, photo:"")
+
 Senior.create(name:"Geargette", address:"16 villa gaudelet", city: "paris", key_skill:"street fighter", user_id:"1", age:74, photo:"")
+
 Senior.create(name:"georges", address:"16 villa gaudelet", city: "paris", key_skill:"cusine", user_id:"1", age:74, photo:"")
+
 Senior.create(name:"georges", address:"16 villa gaudelet", city: "paris", key_skill:"balades en foret", user_id:"1", age:74, photo:"")
+
 Senior.create(name:"georges", address:"16 villa gaudelet", city: "paris", key_skill:"course a pied", user_id:"1", age:74, photo:"")
+
 Senior.create(name:"georges", address:"16 villa gaudelet", city: "paris", key_skill:"L.O.L", user_id:"1", age:74, photo:"")
 puts 'création des seniors finie'
 
