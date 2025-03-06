90a395dd79c83d81bac6659d952d05d3f7b75a53
benoit hainaut <benoit.hainaut@gmail.com>
2025-03-06 14:30:10 +0100

    delta inconnu

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2025_03_06_095511) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_03_06_102341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_06_095511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "datetime"
+    t.string "status"
     t.index ["senior_id"], name: "index_bookings_on_senior_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -70,9 +69,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_06_095511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "city"
-    t.text "summary"
     t.float "latitude"
     t.float "longitude"
+    t.text "summary"
     t.index ["user_id"], name: "index_seniors_on_user_id"
   end
 
